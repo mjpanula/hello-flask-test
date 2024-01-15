c6a33df97b7f617839d1a2b45521cde050b2b37e
mpan <Matti.Panula@seamk.fi>
2024-01-15 13:19:16 +0200

    heippa ruotsiksi

--- helloflask.py
+++ helloflask.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def helloworld():
-    return "Heippa maailma"
+    return "Hejsan världen"
 if __name__ == "__main__":
     app.run()
